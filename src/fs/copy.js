254b2d49ede5927193207aa82a563a40294b0db5
Mariya <m.lebadina@freeplay.io>
2024-01-28 20:53:27 +0300

    feat: fs subtask is done

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,25 @@
+import fs from 'fs';
+import path from 'path';
+import {fileURLToPath} from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const pathFrom = path.resolve(__dirname, 'files');
+const pathTo = path.resolve(__dirname, 'files_copy');
+
 const copy = async () => {
-    // Write your code here 
+    if (!fs.existsSync(pathFrom) || fs.existsSync(pathTo)) {
+        throw new Error('FS operation failed')
+    } else {
+        fs.mkdir(pathTo, (err) => {
+            if (err) throw err;
+            fs.readdirSync(pathFrom).forEach(fileName => {
+                const fileBuffer = fs.readFileSync(`${pathFrom}/${fileName}`);
+                fs.writeFileSync(`${pathTo}/${fileName}`, fileBuffer);
+            })
+        })
+    }
 };
 
 await copy();
--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,18 @@
+import fs from 'fs';
+import path from 'path';
+import {fileURLToPath} from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const filePath = path.resolve(__dirname, 'files', 'fileToRemove.txt');
+
 const remove = async () => {
-    // Write your code here 
+    if (!fs.existsSync(filePath)) {
+        throw new Error('FS operation failed')
+    } else {
+        fs.rmSync(filePath);
+    }
 };
 
 await remove();
